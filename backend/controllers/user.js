9ef90cbfc9b76c1fb8589200f089beaaf5ff1f2a
sandeep0394 <saisandeep2606@gmail.com>
2024-12-16 16:47:23 +0530

    1-update paths

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,20 +3,20 @@ import {v2 as cloudinary} from 'cloudinary';
 import user from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
 import User from "../models/user.model.js";
-export const getUserprofile = async (req, res)=>{
- const {username } = req.params;
- try {
-  const user = await user.findOne({username}).select(".password");
-  if (!user){
-    return res.status(404).json({message: "user not found"})
-  }  
-  res.status(200).json(user);
- } catch (error) {
-    res.status(500).json({error:error.message});
+export const getUserprofile = async (req, res) => {
+  const { username } = req.params;
+  try {
+    const user = await User.findOne({ username }).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
     console.log("Error in getUserProfile:", error.message);
-    
- } 
+    res.status(500).json({ error: error.message });
+  }
 };
+
 export const followUnfollowUser = async (req, res)=>{
     try {
        const {id} = req.params;
