889bd6f52baf314e47bfb81c9e780d38e933c834
sandeep0394 <saisandeep2606@gmail.com>
2024-12-23 11:42:53 +0530

    3-fetch posts for the home page

--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -3,43 +3,46 @@ import { Link } from "react-router-dom";
 
 import XSvg from "../../../components/svgs/X";
 
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { MdOutlineMail, MdPassword } from "react-icons/md";
+import { MdOutlineMail } from "react-icons/md";
+import { MdPassword } from "react-icons/md";
 
-import { toast } from "react-hot-toast";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 const LoginPage = () => {
 	const [formData, setFormData] = useState({
 		username: "",
 		password: "",
 	});
-    const queryClient = useQueryClient();
-	const { mutate: loginMutation, isPending, isError, error } = useMutation({
-		mutationFn: async ({ username, password }) => {
-			try{
-			const res = await fetch("/api/auth/login", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({ username, password }),
-			});
-			const data = await res.json();
+	const queryClient = useQueryClient();
 
-			if (!res.ok) {
-				throw new Error(data.error || "Something went wrong");
+	const {
+		mutate: loginMutation,
+		isPending,
+		isError,
+		error,
+	} = useMutation({
+		mutationFn: async ({ username, password }) => {
+			try {
+				const res = await fetch("/api/auth/login", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({ username, password }),
+				});
+
+				const data = await res.json();
+
+				if (!res.ok) {
+					throw new Error(data.error || "Something went wrong");
+				}
+			} catch (error) {
+				throw new Error(error);
 			}
-		}catch(error){ 
-		 throw new Error(error);
-		}
-			},
-		onSuccess: () => {
-			//refetch the authUser
-			queryClient.invalidateQueries({queryKey: ["authUser"]});
 		},
-		onError: (err) => {
-			// Optional: Log the error for debugging
-			console.error("Login failed:", err);
+		onSuccess: () => {
+			// refetch the authUser
+			queryClient.invalidateQueries({ queryKey: ["authUser"] });
 		},
 	});
 
@@ -53,57 +56,50 @@ const LoginPage = () => {
 	};
 
 	return (
-		<div className="max-w-screen-xl mx-auto flex h-screen">
-			<div className="flex-1 hidden lg:flex items-center justify-center">
-				<XSvg className="lg:w-2/3 fill-white" />
+		<div className='max-w-screen-xl mx-auto flex h-screen'>
+			<div className='flex-1 hidden lg:flex items-center  justify-center'>
+				<XSvg className='lg:w-2/3 fill-white' />
 			</div>
-			<div className="flex-1 flex flex-col justify-center items-center">
-				<form className="flex gap-4 flex-col" onSubmit={handleSubmit}>
-					<XSvg className="w-24 lg:hidden fill-white" />
-					<h1 className="text-4xl font-extrabold text-white">{"Let's"} go.</h1>
-					<label className="input input-bordered rounded flex items-center gap-2">
+			<div className='flex-1 flex flex-col justify-center items-center'>
+				<form className='flex gap-4 flex-col' onSubmit={handleSubmit}>
+					<XSvg className='w-24 lg:hidden fill-white' />
+					<h1 className='text-4xl font-extrabold text-white'>{"Let's"} go.</h1>
+					<label className='input input-bordered rounded flex items-center gap-2'>
 						<MdOutlineMail />
 						<input
-							type="text"
-							className="grow"
-							placeholder="username"
-							name="username"
+							type='text'
+							className='grow'
+							placeholder='username'
+							name='username'
 							onChange={handleInputChange}
 							value={formData.username}
 						/>
 					</label>
 
-					<label className="input input-bordered rounded flex items-center gap-2">
+					<label className='input input-bordered rounded flex items-center gap-2'>
 						<MdPassword />
 						<input
-							type="password"
-							className="grow"
-							placeholder="Password"
-							name="password"
+							type='password'
+							className='grow'
+							placeholder='Password'
+							name='password'
 							onChange={handleInputChange}
 							value={formData.password}
 						/>
 					</label>
-					<button className="btn rounded-full btn-primary text-white">
+					<button className='btn rounded-full btn-primary text-white'>
 						{isPending ? "Loading..." : "Login"}
 					</button>
-					{isError && (
-						<p className="text-red-500">
-							{error?.message || "Something went wrong. Please try again."}
-						</p>
-					)}
+					{isError && <p className='text-red-500'>{error.message}</p>}
 				</form>
-				<div className="flex flex-col gap-2 mt-4">
-					<p className="text-white text-lg">{"Don't"} have an account?</p>
-					<Link to="/signup">
-						<button className="btn rounded-full btn-primary text-white btn-outline w-full">
-							Sign up
-						</button>
+				<div className='flex flex-col gap-2 mt-4'>
+					<p className='text-white text-lg'>{"Don't"} have an account?</p>
+					<Link to='/signup'>
+						<button className='btn rounded-full btn-primary text-white btn-outline w-full'>Sign up</button>
 					</Link>
 				</div>
 			</div>
 		</div>
 	);
 };
-
 export default LoginPage;
