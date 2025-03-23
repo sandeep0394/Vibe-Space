f5a7edd8d79dd6d401013e5c9cce9d7e374783e8
Abotula Sai sandeep <145218140+sandeep0394@users.noreply.github.com>
2025-03-23 19:41:44 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
 		port: 3000,
 		proxy: {
 			"/api": {
-				target: "https://vibe-space.onrender.com",
+				target: "http://localhost:5000",
 				changeOrigin: true,
 			},
 		},
