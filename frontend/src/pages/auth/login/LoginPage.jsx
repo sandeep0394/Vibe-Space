0b1d1ded6a98ed1101a092eeca98c0bf1acca621
sandeep0394 <saisandeep2606@gmail.com>
2025-03-23 18:31:39 +0530

    published in render

--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -23,7 +23,7 @@ const LoginPage = () => {
 	} = useMutation({
 		mutationFn: async ({ username, password }) => {
 			try {
-				const res = await fetch("https://vibe-space.onrender.com/api/auth/login", {
+				const res = await fetch("/api/auth/login", {
 					method: "POST",
 					headers: {
 						"Content-Type": "application/json",
